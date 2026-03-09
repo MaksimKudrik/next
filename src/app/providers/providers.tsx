a0909cc26d0c77645b4c3441a1655b0904f0a335
Maksim <kma-snz@mail.ru>
2026-03-09 17:01:18 +0500

    add navbar

---
+++ src/app/providers/providers.tsx
@@ -0,0 +1,8 @@
+"use client";  // ← только здесь!
+
+import { HeroUIProvider } from "@heroui/react";
+import { ReactNode } from "react";
+
+export function Providers({ children }: { children: ReactNode }) {
+  return <HeroUIProvider>{children}</HeroUIProvider>;
+}
